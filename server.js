459e601c1df8d59693e4c2778b857a5531f1e795
sunilkumar25kush <sunilkumar25@navgurukul.org>
2026-05-30 16:29:40 +0530

    Add Vercel configuration for Express server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const app = require("./src/app");
 
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT,function(){
-//     console.log("server chal raha hai")
-// })
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Locally run karte waqt
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+// Vercel ke liye export karo
+module.exports = app;
